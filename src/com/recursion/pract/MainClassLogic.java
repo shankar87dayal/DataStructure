b03d45fa74da2a47ca6c59e974e7e4f6d294f8ac
User <User@DESKTOP-98P96A5>
2021-12-02 20:05:37 +0530

    Updating Fact in main class.

--- src/com/recursion/pract/MainClassLogic.java
+++ src/com/recursion/pract/MainClassLogic.java
@@ -2,8 +2,11 @@ package com.recursion.pract;
 
 public class MainClassLogic {
 	static FibonacciLogic03 objFibo = new FibonacciLogic03();
+	static RecursionLogic01 objFac = new RecursionLogic01();
 
 	public static void main(String[] args) {
 		System.out.println("fibonacci of 5th term = " + objFibo.fib(5));
+		int factorial = objFac.factorial(10);
+		System.out.println("Factorial of 10 is " + factorial);
 	}
 }
--- src/com/recursion/pract/RecursionLogic01.java
+++ src/com/recursion/pract/RecursionLogic01.java
@@ -2,17 +2,10 @@ package com.recursion.pract;
 
 public class RecursionLogic01 {
 
-	static RecursionLogic01 obj = new RecursionLogic01();
-	
-	public static void main(String[] args) {
-		int factorial = obj.factorial(5);
-		System.out.println("Factorial of 5 is "+factorial);
-	}
-
 	public int factorial(int n) {
-		if(n==0 || n==1)
+		if (n == 0 || n == 1)
 			return 1;
 		else
-			return n*factorial(n-1);
+			return n * factorial(n - 1);
 	}
 }
