bf5302eee06a2505c7d7038befccac9849cbf428
User <User@DESKTOP-98P96A5>
2021-12-02 09:49:44 +0530

    recursion logic factorial program

--- src/com/recursion/pract/RecursionLogic01.java
+++ src/com/recursion/pract/RecursionLogic01.java
@@ -2,8 +2,17 @@ package com.recursion.pract;
 
 public class RecursionLogic01 {
 
+	static RecursionLogic01 obj = new RecursionLogic01();
+	
 	public static void main(String[] args) {
-		System.out.println("hai");
+		int factorial = obj.factorial(5);
+		System.out.println("Factorial of 5 is "+factorial);
 	}
 
+	public int factorial(int n) {
+		if(n==0 || n==1)
+			return 1;
+		else
+			return n*factorial(n-1);
+	}
 }
